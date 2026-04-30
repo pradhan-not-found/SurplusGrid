9edf8bd66d4637da6e3a0d76961404264ee55d91
pradhan-not-found <souradeeppradhan7@gmail.com>
2026-04-30 17:34:24 +0530

    fixed issues

--- frontend/src/pages/auth/Onboarding.tsx
+++ frontend/src/pages/auth/Onboarding.tsx
@@ -1,5 +1,5 @@
 import { useState } from 'react';
-import { useAuth } from './context/AuthContext';
+import { useAuth } from '../../context/AuthContext';
 import { Navigate, useNavigate } from 'react-router-dom';
 
 export default function Onboarding() {
